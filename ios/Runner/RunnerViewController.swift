be568cdb9ec9d6f15ea749abb48a32ccb8fd5df6
大祐永田 <dbank0208@gmail.com>
2021-11-13 21:11:45 +0900

    change: desgin color by RunnerViewController

--- ios/Runner/RunnerViewController.swift
+++ ios/Runner/RunnerViewController.swift
@@ -8,13 +8,17 @@ class RunnerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         b.frame = CGRect(x: UIScreen.main.bounds.width/2-50, y: 100, width: 100, height: 50)
-        b.setTitle("Push", for: .normal)
-        titleLabel.frame = b.frame
-        titleLabel.frame.size.width = 200
-        titleLabel.frame.origin.y = 200
-        titleLabel.text = "RunnerViewController"
         b.addTarget(self, action: #selector(action), for: .touchUpInside)
+        b.setTitle("Push", for: .normal)
         b.backgroundColor = .lightGray
+
+        titleLabel.frame = b.frame
+        titleLabel.frame.size.width = UIScreen.main.bounds.width
+        titleLabel.frame.origin.x = 0
+        titleLabel.frame.origin.y = 200
+        titleLabel.textAlignment = .center
+        titleLabel.text = "RunnerViewController"
+
         view.addSubview(b)
         view.addSubview(titleLabel)
         view.backgroundColor = .white
@@ -22,6 +26,5 @@ class RunnerViewController: UIViewController {
 
     @objc func action() {
         titleLabel.text = "Log count" + Int.random(in: 1..<10).description
-        titleLabel.textAlignment = .center
     }
 }
